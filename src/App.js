1198e5e86b0f461f39c0f0c92d0d95c1cace2736
yuriya <namyuri@gmail.com>
2021-12-05 20:01:32 +0900

    add public url for github page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./routes/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
