2c856c08db030c7fa55e9441cb16537ebd8e1000
yuriya <namyuri@gmail.com>
2021-12-18 21:21:59 +0900

    #4 add version in footer

--- src/components/HomeFooter.js
+++ src/components/HomeFooter.js
@@ -4,9 +4,12 @@ import { Link } from "react-router-dom";
 function HomeFooter() {
   return (
     <div className={styles.homefooter}>
-      <a href="https://github.com/yuriyaya/lunch-at#readme" target="_blank">
-        about
-      </a>
+      <div>
+        <a href="https://github.com/yuriyaya/lunch-at#readme" target="_blank">
+          <b>about</b>
+        </a>
+      </div>
+      <div>version: 0.2.0</div>
     </div>
   );
 }
